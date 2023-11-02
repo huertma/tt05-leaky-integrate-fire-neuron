289d6b24c1d833a39b64a7c6067f8c71d302f62e
huertma <mariana.e.huerta@gmail.com>
2023-11-02 16:05:02 -0700

    update lif.v

--- src/lif.v
+++ src/lif.v
@@ -3,7 +3,7 @@
 module lif ( 
     input wire [7:0] current,
     input wire       clk,
-    input wire       rst_n,
+    input wire       rst_n, // active low, RESET happens when rst_n is 0
     output wire      spike,
     output reg [7:0] state
 );
@@ -24,9 +24,9 @@ module lif (
     // assign next_state = current + (spike ? 0 : state >> 1)
     // if there is a spike, next state is current + 0; if no spike, next state is current + half of state
 
-    assign next_state = (spike ? 8'b0 : current) + (spike ? 0 : (state >> 1)+(state >> 2)+(state >> 3));
+    assign next_state = (spike ? 0 : current) + (spike ? 0 : (state >> 1)+(state >> 2)+(state >> 3));
     // first term: if there is a spike, dont add current. if no spike, add current
-    // second term: if yes spike, add zero; if no spike, add 0.875 times state
+    // second term: if yes spike, add zero; if no spike, add 0.875 times state (this implicitly sets Beta decay rate)
     // combine the two terms: if yes spike, HARD RESET (both terms eval to zero)
 
 endmodule
